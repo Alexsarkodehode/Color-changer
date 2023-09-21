2c6685dcf470ea93295a8bb3b3605e683ebccfad
Alexsarkodehode <alexsar.kodehode@gmail.com>
2023-09-21 02:14:56 +0200

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let index = 0;
+
+function changeColors () {
+     const colors = ["red", "pink", "beige", "yellow", "purple", "brown"];
+
+     document.getElementsByTagName("body")[0].style,background = colors[index++];
+
+     if(index > colors.lenght -1)
+}
